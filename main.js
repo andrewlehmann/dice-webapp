2ef6fc83c6f2337011068f7dec2164e5759fb375
andrewlehmann <andrew@arlehmann.com>
2017-01-30 18:38:43 -0600

    more js work

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function rollDice()	{
 
-	var numOfDice = document.getElementByID('dice-count-field').value;
-	var sides = getRadioVal(document.getElementByID('button-panel'), 'dice');
+	var numOfDice = document.getElementById('dice-count-field').value;
+	var sides = getRadioVal(document.getElementById('form1'), 'dice');
 	var returnString;
 	var value;
 	var results = [];
+	console.log("beginning js");
 	for(var i =0; i<numOfDice; i++)	{
 		value = Math.random() % sides;
 		// push value into database
@@ -12,11 +13,18 @@ function rollDice()	{
 	}
 	if(results.length === 1)	{
 		printResults(results[0].toString());
+		console.log("1 die");
+		return;
 	}
 	else  {
 		for(var i = 0; i < results.length; i++)	{
-			returnString += results[i] + ' + ';
+			returnString += results[i].toString() + ' + ';
 		}
+		returnString += ' = ' + results.reduce(add, 0);
+		printResults(returnString);
+		document.getElementById('dice-count-field').value = "";
+		console.log("2 die");
+		return;
 	}
 }
 
@@ -24,6 +32,7 @@ function getRadioVal(form, name)	{
 
 	var val;
 	var radios = form.elements[name];
+	console.log("inside getRadioVal");
 	for (var i=0; i<radios.length; i++)	{
 		if (radios[i].checked)	{
 			val = radios[i].value;
@@ -35,5 +44,5 @@ function getRadioVal(form, name)	{
 
 function printResults(resultString)	{
 
-	document.getElementByID('output').value = resultString;
+	document.getElementById('output').value = resultString;
 }
