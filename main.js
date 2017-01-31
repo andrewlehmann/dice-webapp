621f8efc97fcb8d8cb2fcc079bb729b176755dce
Andrew Lehmann <andrewlehmann@users.noreply.github.com>
2017-01-31 00:59:01 -0600

    add documentation

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ function rollDice()	{
 	var results = [];
 
 	for(var i =0; i<numOfDice; i++)	{
-		value = Math.floor(Math.random() * (sides) + 1);
-		results.push(value);
+		value = Math.floor(Math.random() * (sides) + 1); //calculate result of dice roll
+		results.push(value);	//append to an array of the results
 	}
-	returnString = results[0].toString();
+	returnString = results[0].toString(); //initialize return string to first roll's result
 	if(results.length === 1)	{
 		printResults(returnString);
 		return;
@@ -26,7 +26,7 @@ function rollDice()	{
 function getRadioVal(form, name)	{
 
 	var val;
-	var input = document.getElementsByName("dice");
+	var input = document.getElementsByName("dice"); //get radio buttons
 	var inputList = Array.prototype.slice.call(input);
 	for (var i = 0; i < inputList.length; i++)	{
 		if(inputList[i].checked)	{
@@ -39,12 +39,12 @@ function getRadioVal(form, name)	{
 function createString(results, returnString) {
 	for(var i = 1; i < results.length; i++)	{
 			returnString +=  ' + ' + results[i].toString();
-		}
+		}	//get the sum of all the elements of the array, and append to the end of the return string
 		returnString += ' = ' + results.reduce(function(total, num) { return total + num;});
 		return returnString;
 }
 
-function printResults(resultString)	{
+function printResults(resultString)	{	//puts return string in the text box
 
 	document.getElementById('output').value = resultString;
 }
