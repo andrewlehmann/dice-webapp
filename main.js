1ea19f682e3833572bf8170dbcabf4a6600ef181
andrewlehmann <andrew@arlehmann.com>
2017-01-30 19:34:08 -0600

    fixed random num only including either 20 or 1

--- main.js
+++ main.js
@@ -5,36 +5,36 @@ function rollDice()	{
 	var returnString;
 	var value;
 	var results = [];
-	console.log(numOfDice);
-	console.log("sides: " + sides);
+
 	for(var i =0; i<numOfDice; i++)	{
-		value = Math.ceil(Math.random() * (sides - 1) + 1);
-		console.log(value.toString());
+		value = Math.floor(Math.random() * (sides) + 1);
 		results.push(value);
 	}
 	if(results.length === 1)	{
 		printResults(results[0].toString());
-		console.log("1 die");
 		return;
 	}
 	else  {
-			returnString = results[0].toString();
-		for(var i = 1; i < results.length; i++)	{
+		returnString = results[0].toString();
+		returnString = createString(results, returnString);
+		printResults(returnString);
+		//document.getElementById('dice-count-field').value = "";
+		return;
+	}
+}
+
+function createString(results, returnString) {
+	for(var i = 1; i < results.length; i++)	{
 			returnString +=  ' + ' + results[i].toString();
 		}
 		returnString += ' = ' + results.reduce(function(total, num) { return total + num;});
-		printResults(returnString);
-		//document.getElementById('dice-count-field').value = "";
-		console.log("2 dice");
-		return;
-	}
+		return returnString;
 }
 
 function getRadioVal(form, name)	{
 
 	var val;
 	var input = document.getElementsByName("dice");
-	console.log("inside getRadioVal");
 	var inputList = Array.prototype.slice.call(input);
 	for (var i = 0; i < inputList.length; i++)	{
 		if(inputList[i].checked)	{
